a2dcbc180c87418a260bf8aeeeb4c469d3c9c749
mananuf <bmananaf@gmail.com>
2025-10-15 05:19:37 +0100

    update: adjusts lint rules for migrations and models

--- eslint.config.js
+++ eslint.config.js
@@ -19,14 +19,33 @@ export default tseslint.config(
     },
   },
   perfectionist.configs["recommended-natural"],
-  {
+    {
     files: ["**/*.test.ts", "**/*.spec.ts"],
-    plugins: {
-      vitest,
-    },
+    plugins: { vitest },
     rules: {
       ...vitest.configs.recommended.rules,
       "@typescript-eslint/unbound-method": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+    },
+  },
+  {
+    files: ["src/migrations/**/*.ts"],
+    rules: {
+      "@typescript-eslint/no-unsafe-call": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
+    },
+  },
+    {
+    files: ["src/models/**/*.ts"],
+    rules: {
+            "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
     },
   },
 );
